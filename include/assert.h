3da3cad16935ab64018acc224f573d23058da399
Rodrigo Souza <rsouza01@gmail.com>
2019-07-01 19:37:36 +0200

    Commit before deleting files

--- include/assert.h
+++ include/assert.h
@@ -1,18 +1,13 @@
-#ifndef _ASSERT_H
-#define _ASSERT_H
-
 #ifdef assert
 # undef assert
 #endif
 
-void _assert_fail(const char*, int, const char*, int, const char*);
-
 #ifdef NDEBUG
-# define assert(expression)   ((void)0)
+    #define assert(expression)   ((void)0)
 #else
-# define assert(expression)  \
-  ((void)((expression) ? 0 : (_assert_fail(#expression, 0, __FILE__, __LINE__, NULL), 0)))
-#endif
 
+    void __assert_fail(const char* expression, int errnum, const char* file, int line, const char* func);
 
+    # define assert(expression)  \
+        ((void)((expression) ? (void)0 : (_assert_fail(#expression, 0, __FILE__, __LINE__, NULL), 0)))
 #endif
--- src/assert.c
+++ src/assert.c
@@ -1,8 +1,7 @@
 #include <assert.h>
 
 
-void __assert_fail(const char* expression, int errnum, const char* file, int line, const char* func)
-{
+void __assert_fail(const char* expression, int errnum, const char* file, int line, const char* func) {
 
 
   //abort();
